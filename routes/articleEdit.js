bf821ff79c761ad6b06df1f09cfa60b991f5ad75
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-06-01 18:55:17 +0900

    console.logなどの余分な文を削除

--- routes/articleEdit.js
+++ routes/articleEdit.js
@@ -5,19 +5,18 @@ const { format } = require("date-fns");
 
 router.post("/", async (req, res) => {
   const formatDate = format(new Date(), "yyyy/MM/dd");
-  //   const articleId = req.params.articleId;
   const body = req.body;
-  console.log(body);
   const contentCount = body.body.length;
   let connection;
   try {
     connection = await transaction.beginTransaction();
-    const result = await transaction.executeQuery(
-      connection,
-      await sql("UPDATE_ARTICLE"),
-      [body.title, body.summary, body.imgPath, formatDate, body.articleId]
-    );
-    console.log(result);
+    await transaction.executeQuery(connection, await sql("UPDATE_ARTICLE"), [
+      body.title,
+      body.summary,
+      body.imgPath,
+      formatDate,
+      body.articleId,
+    ]);
     await transaction.executeQuery(
       connection,
       await sql("DELETE_CONTENT_BY_ORDER_NUMBER"),
@@ -28,7 +27,6 @@ router.post("/", async (req, res) => {
       await sql("SELECT_COUNT_CONTENT_BY_ARTICLE_ID"),
       [body.articleId]
     );
-    console.log(Object.values(result2[0])[0]);
     const contentCountByDb = Object.values(result2[0])[0];
     for (const content of body.body) {
       if (content.orderNumber <= contentCountByDb) {
